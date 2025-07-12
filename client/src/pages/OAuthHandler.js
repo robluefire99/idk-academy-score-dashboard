9bc4002a94434e112f4e688edca43af0ab10e6f9
robluefire99 <rezha.law@gmail.com>
2025-07-12 23:33:29 +0700

    update OAuthHandler

--- client/src/pages/OAuthHandler.js
+++ client/src/pages/OAuthHandler.js
@@ -1,19 +1,17 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useSearchParams, useNavigate } from 'react-router-dom';
-import { login, loadMe } from '../redux/authSlice';
+import { loadMe } from '../redux/authSlice';
 
 export default function OAuthHandler() {
   const [params] = useSearchParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector(state => state.auth.user);
 
   useEffect(() => {
     const token = params.get('token');
     if (token) {
       localStorage.setItem('token', token);
-      dispatch(login.fulfilled({ token, id: null, name: null, role: null }));
       dispatch(loadMe()).then(res => {
         const userData = res.payload;
         if (userData && userData.profileComplete === false) {
